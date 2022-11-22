82a60fd2cbdf50b51ecf155dc17f8d0d29168dec
Gehard <gehardnjoro@gmail.com>
2022-11-22 15:27:38 +0300

    UI tests

--- app/src/main/java/com/example/diabetesselfcare/MainActivity.kt
+++ app/src/main/java/com/example/diabetesselfcare/MainActivity.kt
@@ -1,43 +1,16 @@
 package com.example.diabetesselfcare
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.diabetesselfcare.ui.theme.DiabetesSelfcareTheme
+import androidx.core.view.WindowCompat
+import com.google.samples.apps.diabetessSelf.ui.DiaBetessSelf
 
-class MainActivity : ComponentActivity() {
+class DiaBetessSelfActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DiabetesSelfcareTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DiabetesSelfcareTheme {
-        Greeting("Android")
+        setContent { DiaBetessSelf() }
     }
 }
